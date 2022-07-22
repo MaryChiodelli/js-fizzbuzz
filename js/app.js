6a3145e5b88cc9feef26ed038e04ed87e8d7e025
MaryChiodelli <marychiodelli97@gmail.com>
2022-07-22 15:22:37 +0200

    print number from 1 to 100 in the console

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log('hello');
+// PER ogni numero che va da 1 a 100
+for (let i = 1; i <= 100; i++) {
+    // stampare il numero nella console
+    console.log(i);
+}
+
+    // SE il numero è un multiplo di 3
+       // stampare la stringa 'Fizz'
+
+    // SE il numero è un multiplo di 5
+        // stampare la stringa 'Buzz'
+
+    // SE il numero è un multiplo di 3 e 5
+        // stampare la stringa 'FizzBuzz'
