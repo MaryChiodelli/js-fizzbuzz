cc8ef370e3567dd5276aec18c8e026abc55f5c10
MaryChiodelli <marychiodelli97@gmail.com>
2022-07-22 16:19:32 +0200

    add divs from JavaScript

--- bonus-1/js/app.js
+++ bonus-1/js/app.js
@@ -1,19 +1,27 @@
+// selezionare il container
+const containerElement = document.querySelector('.container');
+
 // PER ogni numero che va da 1 a 100
 for (let i = 1; i <= 100; i++) {
     const number = i;
+    // creare un div
+    const divElement = document.createElement('div');
     
-    // SE il numero è un multiplo di 3 e 5
-    if (number % 3 === 0 && number % 5 === 0) {
+    // inserire il contenuto del div
+    if (number % 3 === 0 && number % 5 === 0) {    // SE il numero è un multiplo di 3 e 5
         // stampare la stringa 'FizzBuzz'
-        console.log('FizzBuzz');
+        divElement.append('FizzBuzz');
     } else if (number % 3 === 0) {    // SE il numero è un multiplo di 3
         // stampare la stringa 'Fizz'
-        console.log('Fizz');
+        divElement.append('Fizz');
     } else if (number % 5 === 0) {    // SE il numero è un multiplo di 5
         // stampare la stringa 'Buzz'
-        console.log('Buzz');
+        divElement.append('Buzz');
     } else {
         // stampare il numero nella console
-        console.log(number); 
+        divElement.append(number); 
     }
+    
+    // aggiungere il div nella pagina
+    containerElement.append(divElement);
 }
