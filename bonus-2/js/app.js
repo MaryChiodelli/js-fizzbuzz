073762661ecf4ab0b85b5bf4f47c4a14a714177b
MaryChiodelli <marychiodelli97@gmail.com>
2022-07-22 17:23:41 +0200

    add style to divs & responsive

--- bonus-2/js/app.js
+++ bonus-2/js/app.js
@@ -12,15 +12,19 @@ for (let i = 1; i <= 100; i++) {
     if (number % 3 === 0 && number % 5 === 0) {    // SE il numero è un multiplo di 3 e 5
         // stampare la stringa 'FizzBuzz'
         divElement.append('FizzBuzz');
+        divElement.classList.add('fizzbuzz-bg-color')
     } else if (number % 3 === 0) {    // SE il numero è un multiplo di 3
         // stampare la stringa 'Fizz'
         divElement.append('Fizz');
+        divElement.classList.add('fizz-bg-color')
     } else if (number % 5 === 0) {    // SE il numero è un multiplo di 5
         // stampare la stringa 'Buzz'
         divElement.append('Buzz');
+        divElement.classList.add('buzz-bg-color')
     } else {
         // stampare il numero nella console
-        divElement.append(number); 
+        divElement.append(number);
+        divElement.classList.add('number-bg-color')
     }
     
     // aggiungere il div nella pagina
